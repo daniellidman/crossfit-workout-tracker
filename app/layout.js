d6c97da12b57a26371505a5d4d0c94d4fe192b68
Daniel-Lidman <dlidman@gmail.com>
2025-01-30 05:52:42 -0600

    side nav start

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,30 @@
 import '@/app/_styles/globals.css';
+import { DM_Sans } from 'next/font/google';
+import SideNavigation from './_components/SideNavigation';
 
 export const metadata = {
-  title: 'SITE TITLE HERE',
+  title: {
+    template: '%s  |  Crossfit Workout Tracker',
+    default: 'Welcome  |  Crossfit Workout Tracker',
+  },
+  description: 'All in one personal fitness and workout tracker',
 };
+
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${dmSans.className} bg-slate-100`}>
         <header></header>
-        <main>{children}</main>
+        <main className="grid h-full grid-cols-[16rem_1fr] gap-12">
+          {' '}
+          <SideNavigation />
+          {children}
+        </main>
       </body>
     </html>
   );
