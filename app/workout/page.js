a53b0d261eb682e4a923db83709ec6d8e12b0e5c
Daniel-Lidman <dlidman@gmail.com>
2025-02-04 12:53:13 -0600

    somehow made the workout log even uglier

--- app/workout/page.js
+++ app/workout/page.js
@@ -9,7 +9,7 @@ export default function Page() {
     <div>
       <h1 className="text-4xl font-bold">Workout Log</h1>
       <div>Feb 2 - 8</div>
-      <div className="m-2 grid grid-flow-col grid-cols-2 grid-rows-7">
+      <div className="grid-cols-[1em, 1fr] m-2 grid grid-flow-col grid-rows-7">
         <div className="border-2 border-black bg-blue-100 px-3">Sunday</div>
         <div className="border-2 border-black bg-blue-300 px-3">Monday</div>
         <div className="border-2 border-black bg-blue-100 px-3">Tuesday</div>
@@ -45,35 +45,37 @@ export default function Page() {
       <div>
         LAST WEEK <br></br>Jan 26 - Feb 1
       </div>
-      <div className="m-2 grid grid-flow-col grid-cols-2 grid-rows-7">
-        <div className="border-2 border-black bg-blue-100 px-3">Sunday</div>
-        <div className="border-2 border-black bg-blue-300 px-3">Monday</div>
-        <div className="border-2 border-black bg-blue-100 px-3">Tuesday</div>
-        <div className="border-2 border-black bg-blue-300 px-3">Wednesday</div>
-        <div className="border-2 border-black bg-blue-100 px-3">Thursday</div>
-        <div className="border-2 border-black bg-blue-300 px-3">Friday</div>
-        <div className="border-2 border-black bg-blue-100 px-3">Saturday</div>
-        <div className="border-2 border-black bg-blue-100 px-3">
+      <div className="m-2 grid grid-cols-[8em_1fr] grid-rows-7">
+        <WODDate altRow="odd">Sunday</WODDate>
+        <WODList altRow="odd">
           <RestDay />
-        </div>
-        <div className="border-2 border-black bg-blue-300 px-3">
+        </WODList>
+        <WODDate altRow="even">Monday</WODDate>
+        <WODList altRow="even">
           <WOD>4x4 Deadlifts</WOD>
           <WOD>"Rope"</WOD>
-        </div>
-        <div className="border-2 border-black bg-blue-100 px-3">
+        </WODList>
+        <WODDate altRow="odd">Tuesday</WODDate>
+        <WODList altRow="odd">
           <WOD>Skill EMOM</WOD>
           <WOD>"Breakout"</WOD>
-        </div>
-        <div className="border-2 border-black bg-blue-300 px-3">
-          <RestDay />{' '}
-        </div>
-        <div className="border-2 border-black bg-blue-100 px-3">
+        </WODList>
+        <WODDate altRow="even">Wednesday</WODDate>
+        <WODList altRow="even">
           <RestDay />
-        </div>
-        <div className="border-2 border-black bg-blue-300 px-3">
+        </WODList>
+        <WODDate altRow="odd">Thursday</WODDate>
+        <WODList altRow="odd">
           <RestDay />
-        </div>
-        <div className="border-2 border-black bg-blue-100 px-3">Open Gym</div>
+        </WODList>
+        <WODDate altRow="even">Friday</WODDate>
+        <WODList altRow="even">
+          <RestDay />
+        </WODList>
+        <WODDate altRow="odd">Saturday</WODDate>
+        <WODList altRow="odd">
+          <WOD>Open Gym</WOD>
+        </WODList>
       </div>
     </div>
   );
@@ -81,4 +83,24 @@ export default function Page() {
 
 function RestDay() {
   return <div className="inline-flex rounded-md bg-orange-200 p-1">Rest</div>;
+}
+
+function WODList({ children, altRow }) {
+  return (
+    <div
+      className={`col-span-3 col-start-2 m-auto px-3 ${altRow === 'even' ? 'bg-blue-100' : 'bg-blue-300'}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+function WODDate({ children, altRow }) {
+  return (
+    <div
+      className={`col-span-1 m-auto px-3 ${altRow === 'even' ? 'bg-blue-100' : 'bg-blue-300'}`}
+    >
+      {children}
+    </div>
+  );
 }
