bfa95835b0524a2262470554d93f618cc0308b0f
Daniel-Lidman <dlidman@gmail.com>
2025-02-04 06:28:50 -0600

    Workout Log styling

--- app/workout/page.js
+++ app/workout/page.js
@@ -1,7 +1,84 @@
+import WOD from '../_components/WOD';
+
 export const metadata = {
   title: 'Work Out',
 };
 
 export default function Page() {
-  return <h1>Workout Page Page</h1>;
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Workout Log</h1>
+      <div>Feb 2 - 8</div>
+      <div className="m-2 grid grid-flow-col grid-cols-2 grid-rows-7">
+        <div className="border-2 border-black bg-blue-100 px-3">Sunday</div>
+        <div className="border-2 border-black bg-blue-300 px-3">Monday</div>
+        <div className="border-2 border-black bg-blue-100 px-3">Tuesday</div>
+        <div className="border-2 border-black bg-blue-300 px-3">Wednesday</div>
+        <div className="border-2 border-black bg-blue-100 px-3">Thursday</div>
+        <div className="border-2 border-black bg-blue-300 px-3">Friday</div>
+        <div className="border-2 border-black bg-blue-100 px-3">Saturday</div>
+        <div className="border-2 border-black bg-blue-100 px-3">
+          <RestDay />
+        </div>
+        <div className="flex-wrap gap-2 border-2 border-black bg-blue-300 px-3">
+          <WOD>4x4 Deadlifts</WOD>
+          <WOD>"Diane"</WOD>
+        </div>
+        <div className="border-2 border-black bg-blue-100 px-3">
+          <WOD>Press Complex</WOD>
+          <WOD>"Alice"</WOD>
+        </div>
+        <div className="border-2 border-black bg-blue-300 px-3">
+          Add Workout
+        </div>
+        <div className="border-2 border-black bg-blue-100 px-3">
+          Add Workout
+        </div>
+        <div className="border-2 border-black bg-blue-300 px-3">
+          Add Workout
+        </div>
+        <div className="border-2 border-black bg-blue-100 px-3">
+          Add Workout
+        </div>
+      </div>
+
+      <div>
+        LAST WEEK <br></br>Jan 26 - Feb 1
+      </div>
+      <div className="m-2 grid grid-flow-col grid-cols-2 grid-rows-7">
+        <div className="border-2 border-black bg-blue-100 px-3">Sunday</div>
+        <div className="border-2 border-black bg-blue-300 px-3">Monday</div>
+        <div className="border-2 border-black bg-blue-100 px-3">Tuesday</div>
+        <div className="border-2 border-black bg-blue-300 px-3">Wednesday</div>
+        <div className="border-2 border-black bg-blue-100 px-3">Thursday</div>
+        <div className="border-2 border-black bg-blue-300 px-3">Friday</div>
+        <div className="border-2 border-black bg-blue-100 px-3">Saturday</div>
+        <div className="border-2 border-black bg-blue-100 px-3">
+          <RestDay />
+        </div>
+        <div className="border-2 border-black bg-blue-300 px-3">
+          <WOD>4x4 Deadlifts</WOD>
+          <WOD>"Rope"</WOD>
+        </div>
+        <div className="border-2 border-black bg-blue-100 px-3">
+          <WOD>Skill EMOM</WOD>
+          <WOD>"Breakout"</WOD>
+        </div>
+        <div className="border-2 border-black bg-blue-300 px-3">
+          <RestDay />{' '}
+        </div>
+        <div className="border-2 border-black bg-blue-100 px-3">
+          <RestDay />
+        </div>
+        <div className="border-2 border-black bg-blue-300 px-3">
+          <RestDay />
+        </div>
+        <div className="border-2 border-black bg-blue-100 px-3">Open Gym</div>
+      </div>
+    </div>
+  );
+}
+
+function RestDay() {
+  return <div className="inline-flex rounded-md bg-orange-200 p-1">Rest</div>;
 }
