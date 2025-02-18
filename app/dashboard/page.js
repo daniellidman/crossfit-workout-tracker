0efd2ce3ff0073a12aee38b644c240e4712eaa1a
Daniel-Lidman <dlidman@gmail.com>
2025-02-18 12:15:54 -0600

    dashboard updates

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,5 +3,23 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>Dashboard Page</h1>;
+  return (
+    <div>
+      <h1>Dashboard Page</h1>;
+      <div className="flex min-w-[240px] gap-1 p-2 text-base font-normal">
+        <Link
+          href="/workout/new/workout"
+          className="flex items-center rounded-lg p-3 text-start leading-tight outline-none transition-all hover:bg-white hover:bg-opacity-80 focus:bg-white focus:bg-opacity-80 active:bg-white active:bg-opacity-80"
+        >
+          Log a Workout
+        </Link>
+        <Link
+          href="/workout/new/weightlifting"
+          className="flex items-center rounded-lg p-3 text-start leading-tight outline-none transition-all hover:bg-white hover:bg-opacity-80 focus:bg-white focus:bg-opacity-80 active:bg-white active:bg-opacity-80"
+        >
+          Log Weightlifting
+        </Link>
+      </div>
+    </div>
+  );
 }
