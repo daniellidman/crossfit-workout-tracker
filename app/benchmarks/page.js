76567d2bd8985149db54879c4badf64bf2b9aaeb
Daniel-Lidman <dlidman@gmail.com>
2025-03-18 20:34:11 -0500

    update benchmarks list

--- app/benchmarks/page.js
+++ app/benchmarks/page.js
@@ -6,6 +6,53 @@ export default function Page() {
   return (
     <div>
       <p>BENCHMARKS</p>
+      <ul>
+        <li>
+          <p>Murph</p>
+        </li>
+        <li>
+          <p>Back Squat</p>
+        </li>
+        <li>
+          <p>Deadlift</p>
+        </li>
+        <li>
+          <p>Clean & Jerk</p>
+        </li>
+        <li>
+          <p>Chad1000x</p>
+        </li>
+        <li>
+          <p>Fran</p>
+        </li>
+        <li>
+          <p>Helen</p>
+        </li>
+        <li>
+          <p>Run 5K</p>
+        </li>
+        <li>
+          <p>Snatch</p>
+        </li>
+        <li>
+          <p>L1 Benchmark</p>
+        </li>
+        <li>
+          <p>Fight Gone Bad</p>
+        </li>
+        <li>
+          <p>Filthy 50</p>
+        </li>
+        <li>
+          <p>Grace</p>
+        </li>
+        <li>
+          <p>Max Pullup</p>
+        </li>
+        <li>
+          <p>Sprint 400m</p>
+        </li>
+      </ul>
     </div>
   );
 }
